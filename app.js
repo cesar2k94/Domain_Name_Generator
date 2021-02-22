d7293ebd6f9803e38fc7675ce9c8a3787323e800
Julio <juliomederosarias@gmail.com>
2021-02-22 19:28:46 -0300

    version 0.3

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ for (let key of pronoun) {
 
 let element = document.getElementById("list");
 for(let i = 0; i < cadena.length; i++ ){
-let li = document.createElement("LI");
-let text = document.createTextNode(cadena[i]);
-console.log(text);
-li.appendChild(text);
-element.appendChild(li);
+    let li = document.createElement("LI");
+    let text = document.createTextNode(cadena[i]);
+    console.log(text);
+    li.appendChild(text);
+    element.appendChild(li);
 }
