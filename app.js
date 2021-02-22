5d27efde1d7297505a20449b2157c8a6588ddb8a
Julio <juliomederosarias@gmail.com>
2021-02-22 19:26:50 -0300

    version 0.2

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 let cadena = [];
+
 for (let key of pronoun) {
     for (let index of adj) {
         for (const iterator of noun) {
-            console.log(key + index + iterator + ".com");
             cadena.push(key + index + iterator + ".com");
-            console.log(key + index + iterator + ".net");
-            console.log(key + index + "." + iterator);
+            cadena.push(key + index + iterator + ".net");
+            cadena.push(key + index + "." + iterator);
         }
     }
 }
-console.log(cadena);
-document.querySelector("div").innerHTML = JSON.stringify(cadena);
+
+let element = document.getElementById("list");
+for(let i = 0; i < cadena.length; i++ ){
+let li = document.createElement("LI");
+let text = document.createTextNode(cadena[i]);
+console.log(text);
+li.appendChild(text);
+element.appendChild(li);
+}
